68d2b6018121007a7390dba2009a577ea4e181f2
Sewon Kim <sewonkim0black@gmail.com>
2024-03-31 20:36:15 -0400

    Draw Tool: unpack basic functionality complete, doesn't work with moving blocks

--- drawtool/unpack.js
+++ drawtool/unpack.js
@@ -1,5 +1,4 @@
-import { clearCell } from "./configuration.js"
-import { BLOCK_SIZE, BLOCK_OFFSET } from "./configuration.js";
+import { clearCell, BLOCK_SIZE, BLOCK_OFFSET, BLOCK_TYPES } from "./configuration.js";
 
 const fileInput = document.querySelector(".unpack-button")
 
@@ -23,8 +22,13 @@ fileInput.addEventListener("change", () => {
 
         // fill cells from json
         for (const entry of json) {
-            const row = (entry.pos[0] / BLOCK_SIZE) - BLOCK_OFFSET
-            console.log(entry, row)
+            const row = (entry.pos[1] / BLOCK_SIZE) - BLOCK_OFFSET
+            const col = (entry.pos[0] / BLOCK_SIZE) - BLOCK_OFFSET
+            const type = entry.type
+            const cell = document.querySelector(`.grid > div[data-row="${row}"][data-col="${col}"]`)
+
+            cell.className = type
+            cell.style.backgroundColor = BLOCK_TYPES.find(blockType => blockType.name === type).color
         }
     }
     reader.readAsText(file)
