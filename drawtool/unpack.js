c662f60ee38dcdeb90eb1c3b2c3702415c1c2405
Sewon Kim <sewonkim0black@gmail.com>
2024-03-31 20:37:00 -0400

    Unpack functionality does not work with moving blocks!

--- drawtool/unpack.js
+++ drawtool/unpack.js
@@ -22,9 +22,13 @@ fileInput.addEventListener("change", () => {
 
         // fill cells from json
         for (const entry of json) {
+            const type = entry.type
+            if (type === "moving") {
+                continue
+            }
+
             const row = (entry.pos[1] / BLOCK_SIZE) - BLOCK_OFFSET
             const col = (entry.pos[0] / BLOCK_SIZE) - BLOCK_OFFSET
-            const type = entry.type
             const cell = document.querySelector(`.grid > div[data-row="${row}"][data-col="${col}"]`)
 
             cell.className = type
